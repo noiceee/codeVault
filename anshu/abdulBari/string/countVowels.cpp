2abe45c08017159bb682a966b1911a014ac7e15a
Anshu Ranjan <ranshu1601@gmail.com>
2023-08-24 02:00:01 +0530

    Update in countVowel

--- anshu/abdulBari/string/countVowels.cpp
+++ anshu/abdulBari/string/countVowels.cpp
@@ -4,12 +4,19 @@ using namespace std;
 
 int main() {
   char str[] = {"hi my name is ANSHU"};
+  char vowels[] = {'a','e','i','o','u','A','E','I','O','U'};
   int vCount = 0;
   int cCount = 0;
+
   for (int i = 0; str[i] != '\0'; i++) {
-    if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' ||
-        str[i] == 'u' || str[i] == 'A' || str[i] == 'E' || str[i] == 'I' ||
-        str[i] == 'O' || str[i] == 'U') {
+    bool vowelFlag = false;
+    for(int j=0 ; j<10;j++){
+      if(str[i]==vowels[j]){
+        vowelFlag = true;
+        break;
+      }
+    }
+    if (vowelFlag) {
       vCount++;
     } else if ((str[i] >= 65 && str[i] <= 90) ||
                (str[i] >= 97 && str[i] <= 122)) {
